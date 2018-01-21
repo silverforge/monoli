32505a6d34b519850d1a5cbfae3c7b5fb914e41b
Janos Murvai-Gaal <janos.murvaigaal@gmail.com>
2018-01-21 20:13:06 +0100

    tests introduced

---
+++ __tests__/routes.test.ts
@@ -0,0 +1,18 @@
+import * as chai from 'chai';
+import { assert, should, expect } from 'chai';
+import * as chaiHttp from 'chai-http';
+import server from '../dist/app';
+
+describe("test routes", () => {
+    chai.use(chaiHttp);
+
+    it("GET /", () => {
+        chai.request(server)
+            .get('/')
+            .end((err, res) => {
+                // should.not.exist(err);
+                // res.status.should.eql(200);
+                console.log(`BODY ::: ${JSON.stringify(res.body)}`);
+            });
+    });
+});
