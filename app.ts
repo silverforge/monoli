b13f39b80944924f86a1a2b500de57cbc7b93c94
Janos Murvai-Gaal <janos.murvaigaal@gmail.com>
2018-01-21 16:29:20 +0100

    json responses

--- app.ts
+++ app.ts
@@ -5,11 +5,30 @@ export default class App {
     private static _app = new Koa();
 
     public static main() {
-        this._app.use(async ctx => {
-            ctx.body = 'Hello MoNoLi';
-        });
 
-        this._app.listen(3000);        
+        // x-response-time
+        this._app.use(async (ctx, next) => {
+            const start = Date.now();
+            await next();
+            const ms = Date.now() - start;
+            ctx.set('X-Response-Time', `${ms}ms`);
+            ctx.set('Content-Type', 'application/json; charset=utf8');
+        });
+          
+        // logger
+        this._app.use(async (ctx, next) => {
+            const start = Date.now();
+            await next();
+            const ms = Date.now() - start;
+            console.log(`${ctx.method} ${ctx.url} - ${ms}`);
+        });
+        
+        // response
+        this._app.use(async ctx => {
+            ctx.body = '{"greetings": "Hello MoNoLi"}';
+        });
+        
+        this._app.listen(3000);
     }
 }
 
