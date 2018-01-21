daaa5a2714059233bbecb89668c515bd875faf41
Janos Murvai-Gaal <janos.murvaigaal@gmail.com>
2018-01-21 13:14:31 +0100

    copied dead code eliminted

--- typings.d.ts
+++ typings.d.ts
@@ -1,27 +1,4 @@
-declare namespace Models {
-    export interface IAppConfig {
-        authHeaderValue: string;
-        greenhouseBaseUrl: string;
-        elasticBaseUrl: string;
-    }
-
-    export interface IServices {
-        departments: string;
-        jobStages: string;
-    }
-}
-
 declare module "*.json" {
     const value: any;
     export default value;
 }
-
-// declare module "*/app_config.json" {
-//     const value: any;
-//     export default <Models.IAppConfig>value;
-// }
-
-// declare module "*/greenhouse_services.json" {
-//     const value: any;
-//     export default <Models.IServices>value;    
-// }
