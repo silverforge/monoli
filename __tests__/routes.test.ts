6f6b2c246a6fa7a6b104e9857d278a644009b993
Janos Murvai-Gaal <janos.murvaigaal@gmail.com>
2018-01-25 13:58:02 +0100

    koa-body added

--- __tests__/routes.test.ts
+++ __tests__/routes.test.ts
@@ -8,8 +8,10 @@ afterEach(() => {
 describe("routes: index", () => {
     it("should respond as expected", async () => {
         const response = await request(server).get("/");
+
         expect(response.status).toEqual(200);
         expect(response.type).toEqual("application/json");
-        expect(response.body.data).toEqual("Sending some JSON");
+        expect(response.body.version).toEqual("v1.0");
+        expect(response.body.name).toEqual("monoli");
     });
 });
--- app.ts
+++ app.ts
@@ -1,16 +1,19 @@
 import * as Koa from 'koa';
 import * as Router from 'koa-router';
+import * as KoaBody from 'koa-body';
 
 const router = new Router();
 const app = new Koa();
 const PORT = process.env.PORT || 3000;
+
+app.use(KoaBody());
 
 // logger
 app.use(async (ctx, next) => {
     const start = Date.now();
     await next();
     const ms = Date.now() - start;
-    console.log(`${ctx.method} ${ctx.url} - ${ms}`);
+    console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
 });
 
 // x-response-time
@@ -22,10 +25,15 @@ app.use(async (ctx, next) => {
     ctx.set('Content-Type', 'application/json; charset=utf8');
 });
 
-router.get("/", async ctx => {
+router.get("/", async (ctx, next) => {
   ctx.body = {
-    data: "Sending some JSON"
+    version: 'v1.0',
+    name: 'monoli'
   };
+});
+
+router.post("/motiondetected", async (ctx, next) => {
+    console.log(` BODY ::: ${JSON.stringify(ctx.request.body)}`);
 });
 
 app.use(router.routes());
